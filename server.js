f8f56319a062d83478a70bbbbddccf781170e904
jacopobergamin89-jpg <jacopo.bergamin89@gmail.com>
2026-06-21 18:51:28 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path');
 const { Server } = require('socket.io');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 const server = http.createServer(app);
 const io = new Server(server);
 
